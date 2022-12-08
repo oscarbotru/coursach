ac2410ac57155059216c7db14533d001a7343f02
olegmaslov <oleg.gnome@yandex.ru>
2022-12-08 16:49:18 +0300

    new coursach

--- coursach/__init__.py
+++
@@ -1,4 +0,0 @@
-from coursach.sorter import global_sorter
-from coursach.finder import get_by_date
-# TODO: finder (binary)
-# TODO: cache
--- coursach/files_sorter.py
+++
@@ -1,9 +0,0 @@
-def sort_single_file(file_path):
-    pass
-
-
-def file_sorters(batches_count):
-    print('-- start to sorting all files one by one')
-    for i in range(batches_count):
-        print(f'-- start to sort file number: {i}')
-        sort_single_file(f'data/temp/{i + 1}.csv')
